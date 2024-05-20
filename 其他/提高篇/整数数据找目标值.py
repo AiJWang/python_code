f716424028fda92dc3df47064da1c1e2ebdba7b6
wangaijun <wangaijun@belongnetwork.com>
2024-05-20 17:23:18 +0800

    脚本

--- 其他/提高篇/整数数据找目标值.py
+++ 其他/提高篇/整数数据找目标值.py
@@ -4,3 +4,16 @@
 '''
 def get_target(list1,target):
     ss=dict()
+    for i in range(len(list1)):
+        res=list1[i]
+        if num:=ss.get(target-res):
+            return i,num
+        else:
+            ss[res]=i
+    return None
+
+
+list1=[1,4,6,8,23]
+res=get_target(list1,27)
+print(res)
+
